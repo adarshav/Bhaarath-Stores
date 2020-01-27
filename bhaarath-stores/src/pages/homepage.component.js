28d5093a088a42cd77b3315af9d7e5ede590bd80
adarsha <adarshav957@gmail.com>
2020-01-27 21:43:35 +0530

    first thing

---
+++ bhaarath-stores/src/pages/homepage.component.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const HomePage = () => (
+        <div className = "homepage">
+            <div className = "directory-menu">
+                <div className = "menu-item">
+                    <div className = "content">
+                        <h1 className = "title">Clothing</h1>
+                        <span className = "subtitle">Shop now</span>
+                    </div>
+                </div>
+
+                <div className = "menu-item">
+                    <div className = "content">
+                        <h1 className = "title">Electronics</h1>
+                        <span className = "subtitle">Shop now</span>
+                    </div>
+                </div>
+
+                <div className = "menu-item">
+                    <div className = "content">
+                        <h1 className = "title">Mobiles</h1>
+                        <span className = "subtitle">Shop now</span>
+                    </div>
+                </div>
+
+                <div className = "menu-item">
+                    <div className = "content">
+                        <h1 className = "title">Books</h1>
+                        <span className = "subtitle">Shop now</span>
+                    </div>
+                </div>
+
+                <div className = "menu-item">
+                    <div className = "content">
+                        <h1 className = "title">Renting</h1>
+                        <span className = "subtitle">Shop now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+)
+
+export default HomePage;
