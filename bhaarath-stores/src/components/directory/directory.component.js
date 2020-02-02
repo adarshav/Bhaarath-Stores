61aa42954a89b21b4f9873c4c0587fb41f7e0936
adarsha <adarshav957@gmail.com>
2020-02-02 10:10:43 +0530

    added images

--- bhaarath-stores/src/components/directory/directory.component.js
+++ bhaarath-stores/src/components/directory/directory.component.js
@@ -6,7 +6,40 @@ class Directory extends React.Component  {
         super();
 
         this.state = {
-            sections: 
+            sections:  [
+                {
+                  title: 'clothing',
+                  imageUrl: 'https://ibb.co/kBCs9NT',
+                  id: 1,
+                  linkUrl: 'shop/clothing'
+                },
+                {
+                  title: 'electronics',
+                  imageUrl: 'https://ibb.co/LrGP5gg',
+                  id: 2,
+                  linkUrl: 'shop/eletronics'
+                },
+                {
+                  title: 'Mobiles',
+                  imageUrl: 'https://ibb.co/TPh3txt',
+                  id: 3,
+                  linkUrl: 'shop/mobiles'
+                },
+                {
+                  title: 'shoes',
+                  imageUrl: 'https://ibb.co/x1F6WR5',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/shoes'
+                },
+                {
+                  title: 'renting',
+                  imageUrl: 'https://ibb.co/xLRCdqR',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/renting'
+                }
+              ]
         }
     }
 }
