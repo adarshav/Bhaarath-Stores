98605f082fcbde2572b245001a0f263beb995ac3
adarsha <adarshav957@gmail.com>
2020-02-02 23:50:58 +0530

    styling the menu-items

--- bhaarath-stores/src/components/directory/directory.component.js
+++ bhaarath-stores/src/components/directory/directory.component.js
@@ -17,7 +17,7 @@ class Directory extends React.Component  {
                 },
                 {
                   title: 'electronics',
-                  imageUrl: '',
+                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 2,
                   linkUrl: 'shop/eletronics'
                 },
@@ -48,8 +48,8 @@ class Directory extends React.Component  {
         return(
             <div className = "directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id}) => (
-                        <MenuItem key = {id} title = {title} imageUrl = {imageUrl}/>
+                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                        <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size}/>
                     ))
                 }
             </div>
