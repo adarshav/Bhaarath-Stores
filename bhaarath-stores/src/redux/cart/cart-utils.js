a1b7cf449dc438f0015123661422827629cf3855
adarsha <adarshav957@gmail.com>
2020-03-07 14:57:27 +0530

    Implemented AddItem and Removeitem functionality in the checkout page and restrict the user only with 3 items

--- bhaarath-stores/src/redux/cart/cart-utils.js
+++ bhaarath-stores/src/redux/cart/cart-utils.js
@@ -1,5 +1,5 @@
 // utility functions allows us to keep our files clean and organize functions that we may need in multiple files in one location
-const addItemToCart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
     if(existingItem) {
@@ -12,4 +12,17 @@ const addItemToCart = (cartItems, cartItemToAdd) => {
     return [...cartItems, {...cartItemToAdd, quantity:1}]
 }
 
-export default addItemToCart;
+export const removeItemFromCart = (cartItems, cartItemToRemove)  => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id );
+    } else {
+        return cartItems.map(cartItem => 
+            cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+        )
+    }
+}
+// export default addItemToCart;
+// {...cartItem, quantity:cartItem.quantity + 1}
