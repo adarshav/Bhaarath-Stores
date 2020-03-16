2be3b56fd102b04e3b1edf9e2033bdaacf158e23
adarsha <adarshav957@gmail.com>
2020-03-17 00:43:25 +0530

    Advanced routing

--- bhaarath-stores/src/pages/shop/shop.component.js
+++ bhaarath-stores/src/pages/shop/shop.component.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview  from '../../components/collections-overview/collections-overview.components';
+import CollectionPage from '../collection/collection.component';
 
 // class component
-const ShopPage = () => (
-        // const {collections} = this.state;
-    <div className = 'shopPage'>
-        <CollectionsOverview />
-    </div>
-)
+const ShopPage = ({ match }) => {
+    console.log(match);
+    return (
+        <div className = 'shopPage'>
+            <Route exact path = {`${ match.path }`} component = { CollectionsOverview }/>
+            <Route exact path = {`${ match.path }/:collectionId`} component = { CollectionPage } />
+        </div>
+    )
+}
 
 
 export default ShopPage;
--- bhaarath-stores/src/redux/shop/shop.selectors.js
+++ bhaarath-stores/src/redux/shop/shop.selectors.js
@@ -2,10 +2,24 @@ import { createSelector } from 'reselect';
 
 const selectShop = (state) => state.shop;
 
+const COLLECTION_ID_MAP = {
+    hats:1,
+    sneakers:2,
+    jackets:3,
+    womens:4,
+    mens:5
+}
+
 const selectShopData = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollection = collectionUrlParam =>
+    createSelector(
+        [selectShopData],
+        collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    )
 export default selectShopData;
 
 // labour cess newHome
